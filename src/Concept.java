64ebcfd1f481e82c726ff9681d1879e320a73fbc
GunjanPahwa <gunjan19pahwa@gmail.com>
2026-02-02 23:08:00 +0530

    --n vs n--

---
+++ src/Concept.java
@@ -0,0 +1,13 @@
+public class Concept {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        //fun(n--); this will run an infinte loop as it will keep putting value as 5 and then subtracting it
+        fun(--n);
+    }
+}
