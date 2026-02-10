1f1320fef2213717439d5388eb93cdd51ff69139
GunjanPahwa <gunjan19pahwa@gmail.com>
2026-02-10 20:29:43 +0530

    Permutations

---
+++ src/Strings/Permutations.java
@@ -0,0 +1,39 @@
+package Strings;
+import java.util.ArrayList;
+
+public class Permutations {
+    public static void main(String[] args) {
+        permutation("","abc");
+        System.out.println(permutations("","abc"));
+
+    }
+    static void permutation(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+
+        for(int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            permutation(f+ch+s,up.substring(1));
+        }
+    }
+    static ArrayList<String> permutations(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> ans=new ArrayList<>();
+
+        for(int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            ans.addAll(permutations(f+ch+s,up.substring(1)));
+        }
+        return ans;
+    }
+}
--- src/Strings/SubsetDuplicates.java
+++ src/Strings/SubsetDuplicates.java
@@ -33,7 +33,6 @@ public class SubsetDuplicates {
                 outer.add(internal);
 
             }
-
         }
         return outer;
     }
