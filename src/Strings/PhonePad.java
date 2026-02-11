970ef5bd5b24207c5395b451eb99b6add4792a63
GunjanPahwa <gunjan19pahwa@gmail.com>
2026-02-11 15:20:54 +0530

    leetcode 17

--- src/Strings/PhonePad.java
+++ src/Strings/PhonePad.java
@@ -6,6 +6,7 @@ public class PhonePad {
     public static void main(String[] args) {
         pad("","12");
         System.out.println(pad1("","12"));
+        System.out.println(padCount("","12"));
 
     }
     static void pad(String p, String up){
@@ -34,6 +35,20 @@ public class PhonePad {
             ans.addAll(pad1(p+ch,up.substring(1)));
         }
         return ans;
+
+    }
+    static int padCount(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return 1;
+        }
+        int count=0;
+        int digit=up.charAt(0)-'0'; //this will conver '2' into 2
+        for(int i=(digit-1)*3;i<digit*3;i++){
+            char ch=(char)('a'+i);
+            count=count+padCount(p+ch,up.substring(1));
+        }
+        return count;
 
     }
 }
