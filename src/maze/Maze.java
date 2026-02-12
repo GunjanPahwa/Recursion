fde0ea0cf8824fef84e0df3f21d0064cf61dd63e
GunjanPahwa <gunjan19pahwa@gmail.com>
2026-02-12 15:56:48 +0530

    Number of ways to travel in a maze

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,9 +1,13 @@
 package maze;
 
+import java.util.ArrayList;
+
 public class Maze {
     //print the number of ways to travel from point (a,b) to (1,1)
     public static void main(String[] args) {
         System.out.println(count(3,3));
+        path("",3,3);
+        System.out.println(pathRet("",3,3));
 
     }
     static int count(int r, int c){
@@ -13,5 +17,34 @@ public class Maze {
         int left=count(r-1,c);
         int right=count(r,c-1);
         return left+right;
+    }
+
+    //printing the path
+    static void path(String p,int r,int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p+'D',r-1,c);
+        }
+        if (c > 1) {
+            path(p+'R',r,c-1);
+        }
+    }
+    static ArrayList<String> pathRet(String p, int r, int c){
+        if(r==1&&c==1){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list=new ArrayList<>();
+        if(r>1){
+            list.addAll(pathRet(p+'D',r-1,c));
+        }
+        if (c > 1) {
+            list.addAll(pathRet(p+'R',r,c-1));
+        }
+        return list;
     }
 }
